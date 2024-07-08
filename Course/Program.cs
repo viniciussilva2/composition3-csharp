64d42d00650978bcc00ee75c0648bccdc9d9a4b6
Vinicius Silva <vinyoli.vs@gmail.com>
2024-07-08 15:48:51 -0300

    Adicionando comentário
    Explicando chamada principal.

--- Course/Program.cs
+++ Course/Program.cs
@@ -9,6 +9,7 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            // Solicita e lê os dados do cliente
             Console.WriteLine("Enter client data: ");
             Console.Write("Name: ");
             string clientName = Console.ReadLine();
@@ -16,37 +17,46 @@ namespace Course
             string email = Console.ReadLine();
             Console.Write("Birth date (DD/MM/YYYY): ");
             DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            // Solicita e lê os dados do pedido
             Console.WriteLine("Enter order data: ");
             Console.Write("Status: ");
             OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
 
+            // Cria instâncias do cliente e do pedido com os dados fornecidos, passando as váriaveis que receberam os dados, e instânciando nos itens dos construtores.
             Client client = new Client(clientName, email, birthDate);
             Order order = new Order(DateTime.Now, status, client);
 
+            // Solicita o número de itens do pedido e adiciona cada item ao pedido
             Console.Write("How many items to this order? ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine($"Enter #{i} item data:");
+
+                // Solicita e lê os dados de cada item do pedido
                 Console.Write("Product name: ");
                 string productName = Console.ReadLine();
                 Console.Write("Product price: ");
                 double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+                // Cria uma instância do produto
                 Product product = new Product(productName, price);
 
                 Console.Write("Quantity: ");
                 int quantity = int.Parse(Console.ReadLine());
 
+                // Cria uma instância do item do pedido e adiciona ao pedido
                 OrderItem orderItem = new OrderItem(quantity, price, product);
-
                 order.AddItem(orderItem);
             }
 
+            // Exibe o resumo do pedido
             Console.WriteLine();
             Console.WriteLine("ORDER SUMMARY:");
             Console.WriteLine(order);
+
         }
     }
 }
